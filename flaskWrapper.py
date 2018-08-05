05350fad98ea7fdf392e23dff8057e0feaf026ce
yosh <mailboxusm@gmail>
2018-08-05 10:47:16 +0300

    move

---
+++ flaskWrapper.py
@@ -0,0 +1,42 @@
+"""
+Writen by Noam Celermajer and TechUSM
+"""
+import threading
+import flask
+import os , os.path
+
+class fWrapper():
+    def __init__(self, port=9090, host="0.0.0.0"):
+        self.flaskApp = flask.Flask(self.__class__.__name__)
+
+        @self.flaskApp.route("/", defaults={"path": "index"}, methods=["GET", "POST"])
+        @self.flaskApp.route("/<path:path>", methods=["GET", "POST"])
+        def allroute(path):
+            root = "route_" + path.split("/")[0]
+            m = getattr(self, root, self.route_404)
+            return m(path, flask.request, self.flaskApp)
+
+        threading.Thread(target=lambda: self.flaskApp.run(host=host, port=port)).start()
+
+    def route_index(self, path, request, flaskObj):
+        return "add this method to your class <br>def route_index(self,path, request, flaskObj):"
+
+    def route_404(self, path, request, flaskObj):
+        rsp = flask.make_response("Error: 404 Not Found", 404)
+        return rsp
+
+    def downloadFile(self, file,name):
+        return flask.send_file(filename_or_fp=file,attachment_filename=name,as_attachment=True)
+    def redirect(self,url):
+        rsp =flask.make_response(url,303)
+        rsp.headers["Location"]=url
+        return rsp
+    def static(self,file):
+        try:
+            return open(os.path.abspath(file)).read()
+        except Exception as eee:
+            rsp = flask.make_response("<pre>{0}</pre>".format(eee).replace(os.path.abspath(file),"."), 404)
+            return rsp
+
+
+
